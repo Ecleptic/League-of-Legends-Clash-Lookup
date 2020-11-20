96a427b252e3439d2e4f0b9bf66e7aeb80488985
unknown <c.green5201@gmail.com>
2020-11-20 14:18:48 -0600

    Removed hard-coded api key and added question for it

--- main.js
+++ main.js
@@ -1,28 +1,43 @@
+const readline = require("readline");
 const https = require('https')
-const options = {
-    hostname: 'na1.api.riotgames.com',
-    path: '/lol/summoner/v4/summoners/by-name/The%20Crafty%20Corki?api_key=RGAPI-938889fe-a454-44d8-a117-3a90e52626da',
-    method: 'GET'
-}
 
-const req = https.request(options, res => {
-    console.log(`statusCode: ${res.statusCode}`)
+// Setup console read
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-    // Data is received as binary, store it and convert to json
-    let data = [];
-    res.on('data', d => {
-        data.push(d);
+rl.question("api key: ", key =>{getSummonerInfo(key)})
+
+function getSummonerInfo(key) {
+    console.log(key);
+
+
+    const options = {
+        hostname: 'na1.api.riotgames.com',
+        path: '/lol/summoner/v4/summoners/by-name/The%20Crafty%20Corki?api_key=' + key,
+        method: 'GET'
+    }
+
+    const req = https.request(options, res => {
+        console.log(`statusCode: ${res.statusCode}`)
+
+        // Data is received as binary, store it and convert to json
+        let data = [];
+        res.on('data', d => {
+            data.push(d);
+        })
+        .on('end', () => {
+            let buffer = Buffer.concat(data);
+            let strBuffer = buffer.toString('utf8');
+            let json = JSON.parse(strBuffer)
+            console.log(json);
+        });
     })
-    .on('end', () => {
-        let buffer = Buffer.concat(data);
-        let strBuffer = buffer.toString('utf8');
-        let json = JSON.parse(strBuffer)
-        console.log(json);
-    });
-})
 
-req.on('error', error => {
-    console.error(error)
-})
+    req.on('error', error => {
+        console.error(error)
+    })
 
-req.end()
+    req.end()
+}
