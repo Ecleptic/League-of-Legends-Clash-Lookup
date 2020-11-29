2b3ba3918b4b6a32fe0b271f7a0b6e4ec680d050
unknown <c.green5201@gmail.com>
2020-11-29 15:22:36 -0600

    Updated error checking to work with fetch

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,14 @@ import styles from '../styles/Home.module.css'
 import { Clash } from '../lib/Clash.js'
 
 export default function Home() {
-    const apiKey = getApiKey();
-    const clash = new Clash;
-    clash.run("The Crafty Corki", apiKey);
+    const apiKey = process.env.API_KEY;
+    if (apiKey == undefined) {
+        console.log("Please create a .env file and add API_KEY to it");
+    }
+    else {
+        const clash = new Clash;
+        clash.run("The Crafty Corki", apiKey);
+    }
 
     return (
         <div className={styles.container}>
@@ -55,6 +60,7 @@ export default function Home() {
             </div>
             </main>
 
+            {/*
             <footer className={styles.footer}>
             <a
                 href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
@@ -65,15 +71,7 @@ export default function Home() {
                 <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
             </a>
             </footer>
+            */}
         </div>
   )
-}
-
-function getApiKey() {
-    const apiKey = process.env.API_KEY;
-    if (apiKey == undefined) {
-        console.log("Please create a .env file and add API_KEY to it");
-        //process.exit();
-    }
-    return apiKey;
 }
