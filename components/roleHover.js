592a44424992ad9bbb8052b315e635a48d7d58f6
Curtis Green <c.green5201@gmail.com>
2020-12-24 23:37:52 -0600

    Centered hover card

--- components/roleHover.js
+++ components/roleHover.js
@@ -3,7 +3,7 @@ import winrateStyles from '../styles/winrateTable.module.css'
 import { getWinrateColor } from '../lib/StyleUtilities.js'
 import { useState } from 'react'
 
-export function RoleHover({children, champArr}) {
+export function RoleHover({ children, champArr }) {
     const [show, setShow] = useState();
 
     return (
@@ -15,25 +15,28 @@ export function RoleHover({children, champArr}) {
                 {children}
             </div>
             {/* On hover show champs of this role, prevent showing an empty box if the champArr is empty */}
-            {(show && champArr.length != 0) ? 
-                <div className={hoverStyles.overlay}>
-                    <table>
-                        <thead></thead>
-                        <tbody>
-                        {champArr.map((champ) => (
-                            <tr key={champ.name}>
-                                <td>
-                                    <img src={champ.iconUrl} className={winrateStyles.img} alt={champ.name}/>
-                                </td>
-                                <td>{champ.name}</td>
-                                <td>{champ.games}</td>
-                                <td style={getWinrateColor(champ.winrate)}>{champ.winrate}%</td>
-                            </tr>
-                        ))}
-                        </tbody>
-                    </table>
-                </div>
-            : null}
+            {(show && champArr.length != 0) ?
+                <>
+                    <div className={hoverStyles.triangle}></div>
+                    <div className={hoverStyles.overlay}>
+                        <table>
+                            <thead></thead>
+                            <tbody>
+                                {champArr.map((champ) => (
+                                    <tr key={champ.name}>
+                                        <td>
+                                            <img src={champ.iconUrl} className={winrateStyles.img} alt={champ.name} />
+                                        </td>
+                                        <td>{champ.name}</td>
+                                        <td>{champ.games}</td>
+                                        <td style={getWinrateColor(champ.winrate)}>{champ.winrate}%</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </>
+                : null}
         </div>
     )
 }
