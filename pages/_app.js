dc7db2bef2614dd09b54d3864ef3773087e8a2da
Curtis Green <c.green5201@gmail.com>
2020-12-28 17:25:40 -0600

    Now shows loading text between pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,36 @@
 import '../styles/globals.css'
+import Router from 'next/router'
+import { useState, useEffect } from 'react'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }) {
+    const [loading, setLoading] = useState(false);
+
+    // Listen to loading start/end events
+    useEffect(() => {
+        const start = () => setLoading(true);
+        const end = () => setLoading(false);
+
+        Router.events.on("routeChangeStart", start);
+        Router.events.on("routeChangeComplete", end);
+        Router.events.on("routeChangeError", end);
+
+        // Cleanup: Stop listening to the events
+        return () => {
+            Router.events.off("routeChangeStart", start);
+            Router.events.off("routeChangeComplete", end);
+            Router.events.off("routeChangeError", end);
+        }
+    }, []); // No dependecy list
+
+    // Show loading Text between pages
+    return (
+        <>
+            {loading ? (
+                <h1> Loading...</h1>
+            ) : (
+                <Component {...pageProps} />
+            )}
+        </>
+    )
 }
 
-export default MyApp
